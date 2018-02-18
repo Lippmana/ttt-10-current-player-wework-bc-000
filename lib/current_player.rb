a052c0e945cd95921e9322ad0753ab6c1cab78ad
Adam C Lippman <alippman@meetup.com>
2018-02-18 05:11:10 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 def turn_count(board) 
-board = [" "," "," "," "," "," "," "," "," "]
-counter = 0
-turn_count.each do |board[" "]|
-counter +=1
+  board = [" "," "," "," "," "," "," "," "," "]
+  counter = 0
+  turn_count.each do |board[" "]|
+  counter +=1
 end
 end
 
